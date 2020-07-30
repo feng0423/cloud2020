b94b27164d14cb606c3533d4f1de3d8c4615409e
lijianfeng <853403035@qq.com>
2020-07-30 23:19:39 +0800

    config

---
+++ cloud-config-client-3355/src/main/java/com/tank/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.tank.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author: Tank
+ * @Date: 2020/3/13 1:07
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+
+}
